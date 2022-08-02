cca6dbe7d22cfb5af959f73916e0e44977be7b7c
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2022-08-02 13:25:50 +0600

    JDBC cong & Read DB

--- src/main/java/org/learning/spring/test/SpringLearn/App.java
+++ src/main/java/org/learning/spring/test/SpringLearn/App.java
@@ -1,19 +1,34 @@
 package org.learning.spring.test.SpringLearn;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.dao.DataAccessException;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	
-    	ApplicationContext context = new ClassPathXmlApplicationContext("org/learning/spring/test/SpringLearn/beans/beans.xml");
-    	
-        Parrot parrot = (Parrot) context.getBean("parrot");
-        parrot.speek();
+public class App {
+	public static void main(String[] args) {
 
-        ((ClassPathXmlApplicationContext) context).close();
-        
-    }
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"org/learning/spring/test/SpringLearn/beans/beans.xml");
+
+		NoticesDAO noticesDao = (NoticesDAO) context.getBean("noticesDao");
+
+		try {
+			List<Notic> notices = noticesDao.getNotices();
+
+			for (Notic notic : notices) {
+				System.out.println(notic);
+			}
+			
+			System.out.println(noticesDao.getNotice(2));
+
+		} catch (DataAccessException e) {
+			System.out.println(e.getMessage());
+			System.out.println(e.getClass());
+		}
+
+		((ClassPathXmlApplicationContext) context).close();
+
+	}
 }
---
+++ src/main/java/org/learning/spring/test/SpringLearn/NoticesDAO.java
@@ -0,0 +1,67 @@
+package org.learning.spring.test.SpringLearn;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component("noticesDao")
+public class NoticesDAO {
+
+	private NamedParameterJdbcTemplate jdbc;
+
+	@Autowired
+	public void setDataSource(DataSource jdbc) {
+		this.jdbc = new NamedParameterJdbcTemplate(jdbc);
+	}
+
+	public List<Notic> getNotices() {
+
+		return jdbc.query("select * from notices", new RowMapper<Notic>() {
+
+			@Override
+			public Notic mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Notic notic = new Notic();
+
+				notic.setId(rs.getInt("id"));
+				notic.setName(rs.getString("name"));
+				notic.setEmail(rs.getString("email"));
+				notic.setText(rs.getString("text"));
+
+				return notic;
+			}
+
+		});
+	}
+
+	public Notic getNotice(int id) {
+
+		MapSqlParameterSource sqlParamenter = new MapSqlParameterSource();
+
+		sqlParamenter.addValue("id", id);
+
+		return jdbc.queryForObject("select * from notices where id= :id", sqlParamenter, new RowMapper<Notic>() {
+
+			@Override
+			public Notic mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Notic notic = new Notic();
+
+				notic.setId(rs.getInt("id"));
+				notic.setName(rs.getString("name"));
+				notic.setEmail(rs.getString("email"));
+				notic.setText(rs.getString("text"));
+
+				return notic;
+			}
+
+		});
+	}
+
+}
